9c75a05e00f9af5109bf90de70748e1f0437b613
Jesus Hernandez <jhernandez@transportationamerica.com>
2023-05-29 15:37:47 -0500

    creación de modulos

--- AngularP3/src/app/app.module.ts
+++ AngularP3/src/app/app.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './welcome/home/home.component';
+import {AppRoutingModule} from "./app-routing.module";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
